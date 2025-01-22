e0ab81f355e7c70496a8a0105e5641d4cd1ca559
Bernardo <balvesdeoliveira3@gmail.com>
2025-01-21 21:47:27 -0300

    game search by list completed

--- src/main/java/com/devbernardo/dslist/controllers/GameListController.java
+++ src/main/java/com/devbernardo/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devbernardo.dslist.dto.GameListDTO;
+import com.devbernardo.dslist.dto.GameMinDTO;
 import com.devbernardo.dslist.services.GameListServices;
+import com.devbernardo.dslist.services.GameServices;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,8 +20,16 @@ public class GameListController {
 	@Autowired
 	private GameListServices gameListServices;
 	
+	@Autowired
+	private GameServices gameServices;
+	
 	@GetMapping
 	public List<GameListDTO> findAll() {
 		return gameListServices.findAll();
+	}
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId) {
+		return gameServices.findByList(listId);
 	}
 }
