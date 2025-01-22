2a3e5e8dd33a74eb2548796f119ea5e3cde3b159
Bernardo <balvesdeoliveira3@gmail.com>
2025-01-21 21:29:51 -0300

    creating an endpoint to search for items in a list according to the list id

--- src/main/java/com/devbernardo/dslist/services/GameServices.java
+++ src/main/java/com/devbernardo/dslist/services/GameServices.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devbernardo.dslist.dto.GameDTO;
 import com.devbernardo.dslist.dto.GameMinDTO;
 import com.devbernardo.dslist.entities.Game;
+import com.devbernardo.dslist.projections.GameMinProjection;
 import com.devbernardo.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameServices {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
